0d0655d5472b8ea3d65a8430e1e912878d182073
Andrest07 <57931603+Andrest07@users.noreply.github.com>
2021-04-24 21:08:04 +0700

    Add files via upload

--- FSM DFA Menu App Prototype.py
+++ FSM DFA Menu App Prototype.py
@@ -7,7 +7,7 @@ Created on Fri Apr 23 19:42:17 2021
 
 from automata.fa.dfa import DFA
 dfa = DFA(
-    states={'isClose', 'offlineMenu', 'onlineMenu', 'otherDeliveryOptions', 'onSpot', 'book', 'pickUp', 'delivery', 'processFoodBefore', 'processFoodAfter', 'offlinePayment', 'onlinePaymentBefore', 'onlinePaymentAfter', 'done'},
+    states={'isClose', 'offlineMenu', 'onlineMenu', 'otherDeliveryOptions', 'onSpot', 'book', 'pickUp', 'delivery', 'processFoodBefore', 'processFoodAfter', 'offlinePaymentAfter', 'onlinePaymentBefore', 'onlinePaymentAfter', 'done'},
     input_symbols={'0', '1'},
     transitions={
         'isClose': {'0': 'onlineMenu', '1': 'offlineMenu'},
@@ -15,18 +15,25 @@ dfa = DFA(
         'onlineMenu': {'0': 'book', '1': 'otherDeliveryOptions'},
         'otherDeliveryOptions' : {'0': 'pickUp', '1': 'delivery'},
         'onSpot': {'0': 'processFoodBefore', '1': 'processFoodBefore'},
-        'book': {'0': 'onlinePaymentBefore', '1': 'processFoodBefore'},
-        'pickUp': {'0': 'onlinePaymentBefore', '1': 'processFoodBefore'},
-        'delivery': {'0': 'onlinePaymentBefore', '1': 'processFoodBefore'},
-        'processFoodBefore': {'0': 'offlinePaymentAfter', '1' : 'onlinePaymentAfter'},
+        'book': {'0': 'onlinePaymentBefore', '1': 'onlinePaymentBefore'},
+        'pickUp': {'0': 'onlinePaymentBefore', '1': 'onlinePaymentBefore'},
+        'delivery': {'0': 'onlinePaymentBefore', '1': 'onlinePaymentBefore'},
+        'processFoodBefore': {'0': 'offlinePaymentAfter', '1': 'onlinePaymentAfter'},
         'processFoodAfter': {'0': 'done', '1': 'done'},
-        'offlinePayment': {'0': 'processFoodAfter', '1': 'processFoodAfter'},
+        'offlinePaymentAfter': {'0': 'done', '1': 'done'},
         'onlinePaymentBefore': {'0': 'processFoodAfter', '1': 'processFoodAfter'},
         'onlinePaymentAfter': {'0': 'done', '1': 'done'},
-        'done' : {'0' : 'done', '1' : 'done'}
+        'done' : {'0': 'done', '1': 'done'}
     },
     initial_state='isClose',
     final_states={'done'}
 )
 
-dfa.read_input_stepwise('10000')
+input = '10000'
+
+if dfa.accepts_input(input):
+    for i in dfa.read_input_stepwise(input):
+        print(i)
+    print('accepted')
+else:
+    print('rejected')
